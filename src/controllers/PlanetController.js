5cb6dd0f67186dc6b9397bbe98995e972e54748c
VictorDabela <victordabela@gmail.com>
2019-12-18 11:27:30 -0300

    Improve erro messages

--- src/controllers/PlanetController.js
+++ src/controllers/PlanetController.js
@@ -27,7 +27,7 @@ class PlanetController {
             var planet = await Planet.find({ nome: planetName.toLowerCase() });
 
             if (planet.length > 0)
-                res.status(400).json({ success: false, message: `Planeta '${planetName}' já está cadastrado.` });
+                res.status(400).json({ success: false, error: `Planeta '${planetName}' já está cadastrado.` });
             else {
                 const filmesAparicaoPromise = this.getFilmeAparicao(planetName);
                 filmesAparicaoPromise.then(function (result) {
@@ -43,14 +43,14 @@ class PlanetController {
                     return planet;
 
                 }, function (err) {
-                    res.status(400).json({ success: false, message: err.message });
+                    res.status(400).json({ success: false, error: err.message });
                 }).then(function (result) {
 
                     res.json(result);
                 });
             }
         } catch (error) {
-            res.status(400).json( { success: false, message: error.message} );
+            res.status(400).json( { success: false, error: error.message} );
         }
     }
 
@@ -71,12 +71,12 @@ class PlanetController {
             const planet = await Planet.findById(req.params.id);
 
             if (!planet)
-                return res.status(404).json({ success: false, message: `Planeta id=${req.params.id} não encontrado.` });
+                return res.status(404).json({ success: false, error: `Planeta id=${req.params.id} não encontrado.` });
             else
                 return res.json({ success: true, planet });
 
         } catch (error) {
-            return res.status(400).json({ success: false, message: error.message });
+            return res.status(400).json({ success: false, error: error.message });
         }
     }
 
@@ -87,10 +87,10 @@ class PlanetController {
             if (planet && planet.length > 0)
                 return res.json({ success: true, planet });
             else
-                return res.status(404).json({ success: false, message: `Planeta ${req.params.nome.toLowerCase()} não encontrado.` });
+                return res.status(404).json({ success: false, error: `Planeta ${req.params.nome.toLowerCase()} não encontrado.` });
 
         } catch (error) {
-            return res.status(400).json({ success: false, message: error.message });
+            return res.status(400).json({ success: false, error: error.message });
         }
     }
 
@@ -99,16 +99,16 @@ class PlanetController {
             let planet = await Planet.findOne({ nome: req.params.nome.toLowerCase() });
 
             if (!planet) {
-                res.status(404).json({ success: false, message: 'Planeta não encontrado.' })
+                res.status(404).json({ success: false, error: 'Planeta não encontrado.' });
             }
             else {
                 await Planet.deleteOne({ nome: planet.nome });
 
-                res.json({ success: true, message: `Planeta ${planet.nome} removido com sucesso.` })
+                res.json({ success: true, message: `Planeta ${planet.nome} removido com sucesso.` });
             }
 
         } catch (error) {
-            return res.status(400).json({ success: false, message: error.message });
+            return res.status(400).json({ success: false, error: error.message });
         }
     }
 }
